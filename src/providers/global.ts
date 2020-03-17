58a7af6184cffe62419d445cf92364a0ca80295c
Administrator <developer@addoeventi.it>
2020-03-17 10:46:26 +0100

    edit

--- src/providers/global.ts
+++ src/providers/global.ts
@@ -1,4 +1,4 @@
 export class Global {
-    public static ENDPOINT: string = 'http://localhost:3000';
-    // public static ENDPOINT: string = 'https://api.uehue.com';
+    // public static ENDPOINT: string = 'http://localhost:3000';
+    public static ENDPOINT: string = 'https://api.uehue.com';
 }
